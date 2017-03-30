e832f4254984bfccb5f22bdea9f4937da77ce1f3
enricrp8 <enric.rubio@est.fib.upc.edu>
2017-03-30 19:43:45 +0200

    conectat a firebase

--- app/src/main/java/com/chillasso/chillasso/LoginActivity.java
+++ app/src/main/java/com/chillasso/chillasso/LoginActivity.java
@@ -2,12 +2,26 @@ package com.chillasso.chillasso;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText phone_number_editText,password_editText;
+    private CheckBox remember_me_checkBox;
+    private Button sign_up_button,sign_in_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        phone_number_editText = (EditText) findViewById(R.id.phone_number_editText);
+        password_editText = (EditText) findViewById(R.id.password_editText);
+        remember_me_checkBox = (CheckBox) findViewById(R.id.remember_me_checkbox);
+        sign_up_button = (Button) findViewById(R.id.sign_up_button);
+        sign_in_button = (Button) findViewById(R.id.sign_in_button);
+
     }
 }
